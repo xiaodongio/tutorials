6e11a7bed0cb4aa9df0d0f6ae024fff52612eef6
xiaodongio <qq282162962@gmail.com>
2019-05-13 16:59:34 +0800

    netty in action echo

---
+++ netty4me/src/main/java/com/dek/echo/EchoServer.java
@@ -0,0 +1,49 @@
+package com.dek.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoServer(1024).start();
+    }
+
+    private void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(serverHandler);
+                        }
+                    });
+            //异步地绑定服务器；调用 sync()方法阻塞等待直到绑定完成
+            ChannelFuture future = bootstrap.bind().sync();
+            //获取 Channel 的CloseFuture，并且阻塞当前线程直到完成
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+}
