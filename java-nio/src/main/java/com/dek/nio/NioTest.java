6e72de7ce4610952a577a7cf7f0c348ebcc14524
xiaodongio <qq282162962@gmail.com>
2019-06-26 18:20:50 +0800

    nio

---
+++ java-nio/src/main/java/com/dek/nio/NioTest.java
@@ -0,0 +1,11 @@
+package com.dek.nio;
+
+import java.nio.ByteBuffer;
+
+public class NioTest {
+
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(10);
+    }
+
+}
